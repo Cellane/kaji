a022dbfcf3f98f4b8adad691688715e69b3da417
Milan Vit <milanvit@milanvit.net>
2019-10-10 01:17:24 +0900

    💚 Install Passport... better?

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,12 +3,21 @@
 namespace Tests;
 
 use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Passport\Passport;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->artisan('passport:install');
+    }
 
     protected function signIn($user = null)
     {
