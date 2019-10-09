29f87b73a705a2566316973fde53c14bf9509629
Milan Vit <milanvit@milanvit.net>
2019-10-10 07:08:08 +0900

    💚 Is this the way to go?

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,22 +3,12 @@
 namespace Tests;
 
 use App\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Passport\Passport;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use RefreshDatabase;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->artisan('key:generate');
-        $this->artisan('passport:install');
-    }
 
     protected function signIn($user = null)
     {
